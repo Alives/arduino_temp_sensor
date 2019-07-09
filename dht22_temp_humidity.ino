aca12e56b5f1f7709c31044bb03baa86ba0b0418
Elliott Friedman <elliott.friedman@gmail.com>
2019-07-08 18:29:47 -0700

    4.0
    switch to https pushes by default with a timer
    
     - Push data via https.
     - Use millis() for intervals and handle rollovers.
     - Added /reset function (though it probably doesn't work well).
     - Add more metrics to json output.
     - Return non-string json values as their own types (float/int).
     - Changed post interval to >= 5 seconds.

--- dht22_temp_humidity.ino
+++ dht22_temp_humidity.ino
@@ -1,4 +1,12 @@
-/*  3.0:
+/*  4.0:
+ *   - Fixed issue where syslog was sent twice before clearing.
+ *   - Also push log via HTTPS.
+ *   - Clean up includes.
+ *   - POST with sections designated by <> tags and data in json format.
+ *   - Add heap and RSSI metrics.
+ *   - Convert strings to F() where possible.
+ *
+ *  3.0:
  *  - Push data via https.
  *  - Use millis() for intervals and handle rollovers.
  *  - Added /reset function (though it probably doesn't work well).
@@ -10,36 +18,23 @@
  *  - Added WifiManager
  */
 
-#include <Adafruit_Sensor.h>
 #include <ArduinoOTA.h>
-#include <DHT.h>
 #include <DHT_U.h>
-#include <DNSServer.h>
-#include <ESP8266mDNS.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266WiFi.h>
-#include <EspSaveCrash.h>
-#include <FS.h>
 #include <WiFiClientSecure.h>
 #include <WiFiManager.h>
 
 #define DHTPIN 5
 #define DHTTYPE DHT22
-#define HTTP_PORT 80
 #define POST_INTERVAL 5000
 #define VERSION "3.0"
 
 
-const char* host = "<<<<<<<< CHANGE ME --  https-listener.domain.com >>>>>>>>>";
+const char* host = "listener.domain.com" + COMPILER ERROR FORCING USER TO UPDATE
 const int https_port = 443;
 const String sensor_array[] = {
   "c6e1ff", "92ea79", "d65b01", "d658f7", "d65878", "068231"};
 const String sensor_names[] = {
   "1", "2", "kitchen", "travel", "outside", "bedroom"};
-bool setup_complete = false;
-bool wifi_connected = false;
-bool write_graphite_flag = false;
-
 String sensor_name = "";
 String log_str = "";
 
@@ -49,254 +44,264 @@ uint32_t next_post_timestamp = 0L;
 uint32_t temperature_errors = 0L;
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
-ESP8266WebServer HTTPserver(HTTP_PORT);
+ESP8266WebServer http_server(80);
 WiFiClientSecure https_client;
+WiFiManager wifiManager;
 
-
-String timestamp(void) {
-  return "[" + String(millis() / 1000.0) + "] ";
-}
-
-void syslog(const String line) {
-  String entry = timestamp() + line;
+void syslog (const String line) {
+  String entry = '[' + String(millis() / 1000.0) + ']' + ' ' + line;
   Serial.println(entry);
-  log_str += entry + "\n";
+  log_str += entry + '\n';
 }
 
-void setupDHT(void) {
-  sensor_t sensor;
-  String name;
-  String unit;
-
-  dht.begin();
-  syslog("DHT" + String(DHTTYPE) + " Sensor ready.");
-  syslog("------------ DHT Sensor ------------");
-  for (int i = 0; i < 2; i++) {
-    if (i == 0) {
-      dht.temperature().getSensor(&sensor);
-      name = "Temperature";
-      unit = "°C";
-    } else {
-      dht.humidity().getSensor(&sensor);
-      name = "Humidity";
-      unit = "%";
-    }
-    syslog(name + " Sensor:     " + String(sensor.name));
-    syslog(name + " Driver Ver: " + String(sensor.version));
-    syslog(name + " Max Value:  " + String(sensor.max_value) + unit);
-    syslog(name + " Min Value:  " + String(sensor.min_value) + unit);
-    syslog(name + " Resolution: " + String(sensor.resolution) + unit);
-  }
-  syslog("------------------------------------");
-}
-
-String getSensorData(void) {
+float getTemperature () {
   sensors_event_t event;
-  String c;
-  String f;
-  String h;
-  String response = "{";
-
   dht.temperature().getEvent(&event);
   while (isnan(event.temperature)) {
     temperature_errors += 1;
-    syslog("Error reading temperature!");
+    syslog(F("Error reading temperature!"));
     delay(250);
     dht.temperature().getEvent(&event);
   }
 
-  c = String(event.temperature);
-  f = String((event.temperature * 9.0 / 5.0) + 32);
+  return event.temperature;
+}
 
+float getFahrenheit (float c) {
+  return c * 1.8 + 32;
+}
+
+float getHumidity () {
+  sensors_event_t event;
   dht.humidity().getEvent(&event);
   while (isnan(event.relative_humidity)) {
     humidity_errors += 1;
-    syslog("Error reading humidity!");
+    syslog(F("Error reading humidity!"));
     delay(250);
     dht.humidity().getEvent(&event);
   }
-  h = String(event.relative_humidity);
-
-  response +=
-    "\"https_connect_attempts\": " + String(https_connect_attempts) + ", " +
-    "\"humidity_errors\": " + String(humidity_errors) + ", " +
-    "\"humidity\": " + h + ", " +
-    "\"temperature_errors\": " + String(temperature_errors) + ", " +
-    "\"tempC\": " + c + ", " +
-    "\"tempF\": " + f + ", " +
-    "\"uptimeMS\": " + String(millis()) + "}";
-
-  return response;
+  return event.relative_humidity;
 }
 
-void httpRoot(void) {
-  String data = getSensorData();
-  syslog(timestamp() + "HTTP GET " + HTTPserver.uri() + " : " + data);
-  HTTPserver.send(200, "text/plain", data + "\n");
+String getSensorData () {
+  const String comma = F(", ");
+  float c = getTemperature();
+  String data = F("{");
+  data += F("\"free_heap\": ");
+  data += String(ESP.getFreeHeap());
+  data += comma;
+  data += F("\"heap_fragmentation_percent\": ");
+  data += String(ESP.getHeapFragmentation());
+  data += comma;
+  data += F("\"https_connect_attempts\": ");
+  data += String(https_connect_attempts);
+  data += comma;
+  data += F("\"humidity_errors\": ");
+  data += String(humidity_errors);
+  data += comma;
+  data += F("\"humidity\": ");
+  data += String(getHumidity());
+  data += comma;
+  data += F("\"RSSI\": ");
+  data += String(WiFi.RSSI());
+  data += comma;
+  data += F("\"temperature_errors\": ");
+  data += String(temperature_errors);
+  data += comma;
+  data += F("\"tempC\": ");
+  data += String(c);
+  data += comma;
+  data += F("\"tempF\": ");
+  data += String(getFahrenheit(c));
+  data += comma;
+  data += F("\"uptimeMS\": ");
+  data += String(millis());
+  data += F("}");
+  return data;
 }
 
-void notFound(void) {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += HTTPserver.uri();
-  message += "\nMethod: ";
-  message += (HTTPserver.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += HTTPserver.args();
-  message += "\n";
-  for (uint8_t i = 0; i < HTTPserver.args(); i++) {
-    message += " " + HTTPserver.argName(i) + ": " + HTTPserver.arg(i) + "\n";
+void httpRoot () {
+  const String msg = F("HTTP GET ");
+  syslog(msg + http_server.uri());
+  http_server.send(200, F("text/plain"), getSensorData());
+}
+
+void notFound () {
+  String msg = F("File Not Found\n\nURI: ");
+  msg += http_server.uri();
+  msg += F("\nMethod: ");
+  msg += (http_server.method() == HTTP_GET) ? F("GET") : F("POST");
+  msg += F("\nArguments: ");
+  msg += http_server.args();
+  msg += '\n';
+  for (uint8_t i = 0; i < http_server.args(); i++) {
+    msg += ' ' + http_server.argName(i) + ": " + http_server.arg(i) + '\n';
   }
-  HTTPserver.send(404, "text/plain", message);
-  if (HTTPserver.uri() != "/favicon.ico") {
-    syslog("HTTP/404 " + HTTPserver.uri());
+  http_server.send(404, F("text/plain"), msg);
+  if (http_server.uri() != F("/favicon.ico")) {
+    msg = F("HTTP/404 ");
+    syslog(msg + http_server.uri());
   }
 }
 
-void httpLedsOn(void) {
+void httpLedsOn () {
+  const String msg = F("LEDs are on.");
   digitalWrite(0, 0);
   digitalWrite(2, 0);
-  syslog("LEDs are on.");
-  httpLog();
+  syslog(msg);
+  http_server.send(200, F("text/plain"), msg);
 }
 
-void httpLedsOff(void) {
+void httpLedsOff () {
+  const String msg = F("LEDs are off.");
   digitalWrite(0, 1);
   digitalWrite(2, 1);
-  syslog("LEDs are off.");
-  httpLog();
+  syslog(msg);
+  http_server.send(200, F("text/plain"), msg);
 }
 
-void httpLog(void) {
-  String response = "\n" + timestamp() + "Current data: " + getSensorData();
-  HTTPserver.send(200, "text/plain; charset=\"UTF-8\"", log_str + response);
+void httpReset () {
+  const String msg = F("Resetting system...");
+  syslog(msg);
+  http_server.send(200, F("text/plain"), msg);
+  systemReset();
 }
 
-void httpPost(void) {
+void systemReset () {
+  ESP.restart();
+  delay(5000);
+}
+
+void httpPost () {
   postData();
-  httpLog();
+  httpRoot();
 }
 
-void writeGraphite() {
+void writeGraphite () {
   if (millis() >= next_post_timestamp) {
     next_post_timestamp = millis() + POST_INTERVAL;
     postData();
   }
 }
 
-void connectHTTPSClient(void) {
+void connectHTTPSClient() {
+  const String conn = F("Connecting to https://");
   for (;;) {
-    syslog("Connecting to https://" + String(host));
+    syslog(conn + String(host));
     https_client.setInsecure();
     if (https_client.connect(host, https_port)) {
-      syslog("HTTPS client connected!");
+      syslog(F("HTTPS client connected!"));
       https_connect_attempts += 1;
       https_client.keepAlive();
       return;
     } else {
-      syslog("Connection to https://" + String(host) + " failed!");
+      syslog(conn + String(host) + F(" failed!"));
       delay(5000);
     }
   }
 }
 
-void postData(void) {
-  String data = "prefix=temp_sensor." + sensor_name +
-                "&data=" + getSensorData();
-  String data_length = String(data.length());
+void postData () {
+  if (WiFi.status() != WL_CONNECTED) { return; }
+  const String rn = F("\r\n");
+  String data;
+  String headers;
+  data = F("<prefix>temp_sensor.");
+  data += sensor_name;
+  data += F("</prefix><data>");
+  data += getSensorData();
+  data += F("</data><log>");
+  data += log_str;
+  data += F("</log>");
 
   if (!https_client.connected()) {
     connectHTTPSClient();
   } else {
-    https_client.print(String(
-      "POST / HTTP/1.1\r\n") +
-      "Host: " + String(host) + "\r\n" +
-      "User-Agent: " + sensor_name + "\r\n" +
-      "Content-Length: " + data_length + "\r\n" +
-      "Connection: keep-alive\r\n\r\n" + data);
+    headers = F("POST / HTTP/1.1\r\nHost: ");
+    headers += String(host);
+    headers += rn;
+    headers += F("User-Agent: ");
+    headers += sensor_name;
+    headers += rn;
+    headers += F("Content-Length: ");
+    headers += String(data.length());
+    headers += rn;
+    headers += F("Connection: keep-alive\r\n\r\n");
+    https_client.print(headers + data);
+    log_str = F("");  // Clear the syslog.
   }
   if (https_client.available()) {
     String status = https_client.readStringUntil('\r');
     https_client.readString();  // flush the buffer.
-    if (status != "HTTP/1.1 200 OK") {
+    if (status != F("HTTP/1.1 200 OK")) {
       syslog(status);
     }
   }
 }
 
-void setupArduinoOTA(String sensor_name) {
+void setupArduinoOTA (String sensor_name) {
   ArduinoOTA.onStart([]() {
-    syslog("ArduinoOTA Started");
+    syslog(F("ArduinoOTA Started"));
   });
   ArduinoOTA.onEnd([]() {
-    syslog("ArduinoOTA Ended");
+    syslog(F("ArduinoOTA Ended"));
   });
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    syslog("ArduinoOTA Progress: " +
-        String(progress / (total / 100.0)) + "\r");
+    String msg = F("ArduinoOTA Progress: ");
+    msg += String(progress / (total / 100.0));
+    msg += '\r';
+    syslog(msg);
   });
   ArduinoOTA.onError([](ota_error_t error) {
-    String msg = "ArduinoOTA Error[" + String(error) + "]: ";
-    if (error == OTA_AUTH_ERROR) msg += "ArduinoOTA Auth Failed";
-    else if (error == OTA_BEGIN_ERROR) msg += "ArduinoOTA Begin Failed";
-    else if (error == OTA_CONNECT_ERROR) msg += "ArduinoOTA Connect Failed";
-    else if (error == OTA_RECEIVE_ERROR) msg += "ArduinoOTA Receive Failed";
-    else if (error == OTA_END_ERROR) msg += "ArduinoOTA End Failed";
+    String msg = F("ArduinoOTA Error[");
+    msg += String(error);
+    msg += F("]: ");
+    if (error == OTA_AUTH_ERROR) msg += F("ArduinoOTA Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) msg += F("ArduinoOTA Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) msg += F("ArduinoOTA Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) msg += F("ArduinoOTA Receive Failed");
+    else if (error == OTA_END_ERROR) msg += F("ArduinoOTA End Failed");
     syslog(msg);
   });
 
   ArduinoOTA.setHostname(sensor_name.c_str());
   ArduinoOTA.setPassword("password");
   ArduinoOTA.begin();
-  syslog("ArduinoOTA started");
+  syslog(F("ArduinoOTA started"));
 }
 
-void setSensorName(void) {
+void setSensorName () {
   String id = String(ESP.getChipId(), HEX);
-  for (int i = 0; i < sizeof(sensor_array); i++) {
+  for (unsigned int i = 0; i < sizeof(sensor_array); i++) {
     if (sensor_array[i] == id) {
-      sensor_name = String("sensor_" + sensor_names[i]);
+      sensor_name = F("sensor_");
+      sensor_name += sensor_names[i];
       return;
     }
   }
-  syslog("Sensor name detection error!");
+  syslog(F("Sensor name detection error!"));
 }
 
-void httpReset(void) {
-  syslog("Resetting system...");
-  httpLog();
-  systemReset();
-}
+void handleWiFi () {
+  if (WiFi.status() == WL_CONNECTED) { return; }
 
-void systemReset(void) {
-  ESP.reset();
-  delay(5000);
-}
+  String msg;
 
-void handleWiFi(void) {
-  if (WiFi.status() == WL_CONNECTED) {
-    return;
-  }
+  syslog(F("WiFi Reconnecting..."));
 
-  if (setup_complete) {
-    syslog("WiFi disconnected.  Reconnecting...");
-  }
   int onoff = 0;
 
   digitalWrite(2, 0);
-  syslog("Starting WiFi");
 
-  WiFiManager wifiManager;
   wifiManager.setConfigPortalTimeout(30);
-  String ssid = sensor_name + " " + WiFi.macAddress();
+  String ssid = sensor_name + F(" ") + WiFi.macAddress();
   if (!wifiManager.autoConnect(ssid.c_str())) {
-    syslog("failed to connect and hit timeout");
-    delay(3000);
+    syslog(F("Failed to connect to WiFi and hit timeout. Rebooting..."));
     systemReset();
-    delay(5000);
   }
 
-  syslog("Connecting to SSID: " + WiFi.SSID());
+  msg = F("Connecting to SSID: ");
+  syslog(msg + WiFi.SSID());
   while (WiFi.status() != WL_CONNECTED) {
     digitalWrite(2, onoff);
     onoff ^= 1;
@@ -304,60 +309,59 @@ void handleWiFi(void) {
   }
   digitalWrite(2, 1);
 
-  syslog("Connected!");
-  syslog("RSSI: " + String(WiFi.RSSI()) + "dBm");
-  syslog("IP address: " + WiFi.localIP().toString());
-  syslog("Subnet mask: " + WiFi.subnetMask().toString());
-  syslog("Gateway: " + WiFi.gatewayIP().toString());
-  wifi_connected = true;
+  syslog(F("Connected!"));
+  msg = F("RSSI: ");
+  syslog(msg + String(WiFi.RSSI()) + F("dBm"));
+  msg = F("IP address: ");
+  syslog(msg + WiFi.localIP().toString());
+  msg = F("Subnet mask: ");
+  syslog(msg + WiFi.subnetMask().toString());
+  msg = F("Gateway: ");
+  syslog(msg + WiFi.gatewayIP().toString());
 }
 
-void setup(void) {
+void setup () {
+  String msg;
   setSensorName();
   Serial.begin(115200);
   delay(100);
 
   syslog(sensor_name);
-  syslog("Code version " + String(VERSION));
-  syslog("Chip ID: 0x" + String(ESP.getChipId(), HEX));
+  msg = F("Code version ");
+  syslog(msg + String(VERSION));
+  msg = F("Chip ID: 0x");
+  syslog(msg + String(ESP.getChipId(), HEX));
+
+  http_server.on(F("/"), httpRoot);
+  http_server.on(F("/leds_on"), httpLedsOn);
+  http_server.on(F("/leds_off"), httpLedsOff);
+  http_server.on(F("/post"), httpPost);
+  http_server.on(F("/reset"), httpReset);
+  http_server.on(F("/reboot"), httpReset);
+  http_server.onNotFound(notFound);
+  http_server.begin();
+  syslog(F("HTTP server started on port 80"));
 
   pinMode(0, OUTPUT);
   pinMode(2, OUTPUT);
   digitalWrite(0, 1);
 
-  syslog("MAC address: " + WiFi.macAddress());
+  msg = F("MAC address: ");
+  syslog(msg + WiFi.macAddress());
   WiFi.hostname(sensor_name);
 
   handleWiFi();
 
-  setupDHT();
-
-  if (!MDNS.begin(sensor_name)) {
-    syslog("Error setting up MDNS responder!");
-  }
-  syslog("mDNS responder started");
-  MDNS.addService("http", "tcp", 80);
-
   setupArduinoOTA(sensor_name);
 
-  HTTPserver.on("/", httpRoot);
-  HTTPserver.on("/log", httpLog);
-  HTTPserver.on("/leds_on", httpLedsOn);
-  HTTPserver.on("/leds_off", httpLedsOff);
-  HTTPserver.on("/post", httpPost);
-  HTTPserver.on("/reset", httpReset);
-  HTTPserver.on("/reboot", httpReset);
-  HTTPserver.onNotFound(notFound);
-  HTTPserver.begin();
-  syslog("HTTP server started on port " + String(HTTP_PORT));
-
-  setup_complete = true;
+  dht.begin();
+  syslog("DHT" + String(DHTTYPE) + " Sensor ready.");
 }
 
-void loop(void) {
-  ArduinoOTA.handle();
-  HTTPserver.handleClient();
+void loop () {
   handleWiFi();
+  ArduinoOTA.handle();
+  http_server.handleClient();
   writeGraphite();
   yield();
 }
